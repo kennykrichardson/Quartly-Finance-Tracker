dc955250a36af98b3050a0b4c1223983b4183d9a
Ken Richardson <kennykrichardson@gmail.com>
2026-06-19 22:19:37 +0530

    Fix Vite plugin initialization

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    tsconfigPaths,
+    tsconfigPaths(),
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
